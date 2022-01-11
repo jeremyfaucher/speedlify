1d614ecc94ea51af82d3a2d9a557bb4bf95245b5
Jeremy Faucher <cafefaucher@gmail.com>
2022-01-11 15:00:46 -0800

    Update zachleat.js

--- _data/sites/zachleat.js
+++ _data/sites/zachleat.js
@@ -1,6 +1,6 @@
 module.exports = {
-	name: "zachleat.com", // optional, falls back to object key
-	description: "Zach’s Personal web site",
+	name: "design2seo.com", // optional, falls back to object key
+	description: "Jeremy's Personal web site",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
 	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
@@ -13,17 +13,6 @@ module.exports = {
 		freshChrome: "site"
 	},
 	urls: [
-		"https://www.zachleat.com/",
-		"https://www.zachleat.com/web/",
-		"https://www.zachleat.com/web/about/",
-		"https://www.zachleat.com/web/fonts/",
-		"https://www.zachleat.com/web/eleventy/",
-		"https://www.zachleat.com/resume/",
-		"https://www.zachleat.com/twitter/",
-		// Popular Posts
-		"https://www.zachleat.com/web/lighthouse-in-footer/",
-		"https://www.zachleat.com/web/speedlify/",
-		"https://www.zachleat.com/web/comprehensive-webfonts/",
-		"https://www.zachleat.com/web/google-fonts-display/",
+		"https://www.design2seo.com/"
 	]
 };
