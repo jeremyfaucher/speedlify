e7c8febdafdd9380cbce7d814a0e4f000a960cff
Jeremy Faucher <cafefaucher@gmail.com>
2022-01-11 18:36:18 -0800

    Update speedlify.js

--- _data/speedlify.js
+++ _data/speedlify.js
@@ -1,3 +1,5 @@
+const CacheAsset = require("@11ty/eleventy-cache-assets");
+
 module.exports = async function() {
   let url = "https://zealous-lovelace-73ddf1.netlify.app/api/urls.json";
   let json = await CacheAsset(url, {
