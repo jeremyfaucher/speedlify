93154eb6c2d6ac452fa510fddeccf26e604999e4
jeremyfaucher <ashley@Ashleys-MacBook-Pro-2.local>
2022-01-17 15:20:24 -0800

    new setup

---
+++ _data/sites/leaderboard.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "Leaderboard", // optional, falls back to object key
+	description: "Get Speedlify",
+	options: {
+		frequency: 1, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://design2seo.com/",
+		"https://carkeekstudios.com/",
+	]
+};
--- _data/speedlify.js
+++ _data/speedlify.js
@@ -1,7 +1,7 @@
 const CacheAsset = require("@11ty/eleventy-cache-assets");
 
 module.exports = async function() {
-  let url = "https://zealous-lovelace-73ddf1.netlify.app/api/urls.json";
+  let url = "https://getspeedlify.netlify.app/api/urls.json";
   let json = await CacheAsset(url, {
     duration: "1w",
     type: "json",
